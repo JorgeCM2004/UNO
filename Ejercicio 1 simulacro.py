5d2ff09884bde49797e578117d20997002e4ed61
JorgeCM2004 <harryj3699@gmail.com>
2022-11-26 16:28:48 +0100

    uno

--- Ejercicio 1 simulacro.py
+++
@@ -1,9 +0,0 @@
-print('Máxumo común divisor de 3 números')
-num1 = int(input('Primer número:'))
-num2 = int(input('Segundo número:'))
-num3 = int(input('Tercer número:'))
-MCD = 1
-for i in range(1, num1 + 1):
-    if num1 % i == 0 and num2 % i == 0 and num3 % i == 0:
-        MCD = i
-print('El máximo común divisor de', num1, ',', num2, 'y', num3, "es el", MCD)
--- MCD.py
+++
@@ -1,8 +0,0 @@
-a = int(input('Primer número:'))
-b = int(input('Segundo número:'))
-while not a == b:
-    if a > b:
-        a = a - b
-    else:
-        b = b - a
-print('El MCD es:', b)
